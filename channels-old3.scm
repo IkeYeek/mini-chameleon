01978749c22bf3edee36e6de058d9a965dcf89fe
Emmanuel Agullo <emmanuel.agullo@inria.fr>
2021-11-09 20:34:32 +0100

    up-to-date channels (same as in the TP) (sorry for the late commit)

--- channels-old3.scm
+++ channels-old3.scm
@@ -2,7 +2,7 @@
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git")
        (commit
-        "2d2f50b30c4f9a8afd1279a6b97d400802086b04")
+        "c1ca85323d5246fc805a31e03b4164b8da08e930")
        (introduction
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
@@ -12,9 +12,9 @@
        (name 'guix-hpc-non-free)
        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git")
        (commit
-        "c14b2a0231408cb5b30b7b3ba628db168960da44"))
+        "adf3e266fff4ba07b659510cd5c6e307b0a377fb"))
       (channel
        (name 'guix-hpc)
        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
        (commit
-        "f78d16d631736a00a71d031163155f52be0e600d")))
+        "c74e32c148e3d176ff55bb7675a9d4dcb4a02856")))
