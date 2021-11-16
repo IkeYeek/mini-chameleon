4f07707cf1948f6307b2b45c5717224a21b037ca
Emmanuel Agullo <emmanuel.agullo@inria.fr>
2021-11-16 11:56:05 +0100

    New channels for a better clang integration

---
+++ channels-old4.scm
@@ -0,0 +1,20 @@
+(list (channel
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
+       (commit
+        "2d2f50b30c4f9a8afd1279a6b97d400802086b04")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+       (name 'guix-hpc-non-free)
+       (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git")
+       (commit
+        "c14b2a0231408cb5b30b7b3ba628db168960da44"))
+      (channel
+       (name 'guix-hpc)
+       (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
+       (commit
+        "f78d16d631736a00a71d031163155f52be0e600d")))
