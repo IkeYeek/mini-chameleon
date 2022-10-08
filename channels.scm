c0127e31d595201dd444e8f5e22f7431126371f8
Emmanuel Agullo <emmanuel.agullo@inria.fr>
2022-10-09 00:29:29 +0200

    Update channels for 2022

--- channels.scm
+++ channels.scm
@@ -1,20 +1,23 @@
 (list (channel
+       (name 'guix-hpc)
+       (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
+       (branch "master")
+       (commit
+        "5df6c7b8881be1ffcdcbda5c41032f35c2e37f10"))
+      (channel
+       (name 'guix-hpc-non-free)
+       (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git")
+       (branch "master")
+       (commit
+        "07d368150436d64eded315f26a3576ee3bdbc160"))
+      (channel
        (name 'guix)
        (url "https://git.savannah.gnu.org/git/guix.git")
+       (branch "master")
        (commit
-        "122396075f12b013b6bde56dafb895587b95bc9d")
+        "31708431c53524f05e6a0c9fed920cb773e7dd21")
        (introduction
         (make-channel-introduction
          "9edb3f66fd807b096b48283debdcddccfea34bad"
          (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-      (channel
-       (name 'guix-hpc-non-free)
-       (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git")
-       (commit
-        "c14b2a0231408cb5b30b7b3ba628db168960da44"))
-      (channel
-       (name 'guix-hpc)
-       (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
-       (commit
-        "d5f1391f419affbb2889212e0d436069cb0ec473")))
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
