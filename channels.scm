597aaf74f56a4e7e5cadb20df780998bde654f7c
Emmanuel Agullo <emmanuel.agullo@inria.fr>
2022-10-10 12:03:47 +0200

    Channels with new mini-chameleon readily available

--- channels.scm
+++ channels.scm
@@ -1,9 +1,14 @@
 (list (channel
-       (name 'guix-hpc)
-       (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
+       (name 'guix)
+       (url "https://git.savannah.gnu.org/git/guix.git")
        (branch "master")
        (commit
-        "5df6c7b8881be1ffcdcbda5c41032f35c2e37f10"))
+        "5f856c595479c30d9ccdb0063c9124248fdcf5c2")
+       (introduction
+        (make-channel-introduction
+         "9edb3f66fd807b096b48283debdcddccfea34bad"
+         (openpgp-fingerprint
+          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
       (channel
        (name 'guix-hpc-non-free)
        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git")
@@ -11,13 +16,8 @@
        (commit
         "07d368150436d64eded315f26a3576ee3bdbc160"))
       (channel
-       (name 'guix)
-       (url "https://git.savannah.gnu.org/git/guix.git")
+       (name 'guix-hpc)
+       (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
        (branch "master")
        (commit
-        "31708431c53524f05e6a0c9fed920cb773e7dd21")
-       (introduction
-        (make-channel-introduction
-         "9edb3f66fd807b096b48283debdcddccfea34bad"
-         (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
+        "4787b11b13a58f09793aefd9b23fe565d24ed1e9")))
