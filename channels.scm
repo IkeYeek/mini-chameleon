b1d2329c5f28ad2720bad612bf65a076c424f927
Emmanuel Agullo <emmanuel.agullo@inria.fr>
2022-10-10 17:19:37 +0200

    Update of channels.scm for --tune option in mini-chameleon

--- channels.scm
+++ channels.scm
@@ -1,23 +1,23 @@
 (list (channel
-       (name 'guix)
-       (url "https://git.savannah.gnu.org/git/guix.git")
-       (branch "master")
-       (commit
-        "5f856c595479c30d9ccdb0063c9124248fdcf5c2")
-       (introduction
-        (make-channel-introduction
-         "9edb3f66fd807b096b48283debdcddccfea34bad"
-         (openpgp-fingerprint
-          "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "5f856c595479c30d9ccdb0063c9124248fdcf5c2")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
       (channel
-       (name 'guix-hpc-non-free)
-       (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git")
-       (branch "master")
-       (commit
-        "07d368150436d64eded315f26a3576ee3bdbc160"))
+        (name 'guix-hpc-non-free)
+        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git")
+        (branch "master")
+        (commit
+          "07d368150436d64eded315f26a3576ee3bdbc160"))
       (channel
-       (name 'guix-hpc)
-       (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
-       (branch "master")
-       (commit
-        "4787b11b13a58f09793aefd9b23fe565d24ed1e9")))
+        (name 'guix-hpc)
+        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
+        (branch "master")
+        (commit
+          "fb01f04fc643eaa345e92ef0abb215b0c5f08775")))
