abed07e03b31130ad646b4947016f01b0fbcdb80
Alycia Lisito <alycia.lisito@inria.fr>
2023-10-06 13:32:13 +0200

    module update

--- plafrim-formation-env.sh
+++ plafrim-formation-env.sh
@@ -2,23 +2,23 @@
 
 # Outils de base
 module load build/cmake/3.21.3
-module load tools/git/2.30.0
+module load tools/git/2.36.0
 
 # Compilateur
-module load compiler/gcc/9.3.0
+module load compiler/gcc/12.2.0
 module load mpi/openmpi/4.0.3-mlx
-module load compiler/cuda/10.2
+module load compiler/cuda/11.7
 
 # BLAS / LAPACK avec Intel MKL
 module load linalg/mkl/2020_update4
 
 # Gestion du binding des threads et traces (dépendences de StarPU)
-module load hardware/hwloc/2.5.0
-module load trace/fxt/0.3.9
-module load trace/eztrace/1.1-8
+module load hardware/hwloc/2.9.0
+module load trace/fxt/0.3.14
+module load trace/eztrace/1.1-9
 
 # Module StarPU pour la fin du projet (Choose the right one)
-module load runtime/starpu/1.3.8/mpi
+#module load runtime/starpu/1.3.8/mpi
 #module load runtime/starpu/1.3.8/mpi-fxt
 #module load runtime/starpu/1.3.8/mpi-cuda
 #module load runtime/starpu/1.3.8/mpi-cuda-fxt
