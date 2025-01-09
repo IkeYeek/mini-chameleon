690974f58b8990cbb12d6f639a37e2f8ae5d3081
Emmanuel Agullo <emmanuel.agullo@inria.fr>
2025-01-09 01:01:49 +0100

    channels and yml for elementaryx CI/CD

---
+++ .guix-extra/channels-elementaryx.scm
@@ -0,0 +1,17 @@
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "5a95cf76e1d0f9fdff5b232b42337c657b76d1d4")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-hpc)
+        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc.git")
+        (branch "master")
+        (commit
+          "b7608db6ecff32e2569ed8407d62ac1485e2856a")))
