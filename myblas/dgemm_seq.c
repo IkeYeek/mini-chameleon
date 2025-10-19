70bd70451c0dc8e5ac850591ed213c5c757f6878
Léo Paillé <leo.paille@enseirb-matmeca.fr>
2025-10-19 19:24:32 +0200

    feat: dgemm seq with blocks + cmake min ver bump for <PackageName>_ROOT resolve

--- myblas/dgemm_seq.c
+++ myblas/dgemm_seq.c
@@ -13,79 +13,182 @@
  *
  */
 #include "myblas.h"
+#include <stdio.h>
 
 // Exemple of ways to add additionnal parameters to your kernel
 // See the registration function to change its value
 static int dgemm_seq_block_size = -1;
 
-int dgemm_seq( CBLAS_LAYOUT layout, CBLAS_TRANSPOSE transA,
-               CBLAS_TRANSPOSE transB, const int M, const int N,
-               const int K, const double alpha, const double *A,
-               const int lda, const double *B, const int ldb,
-               const double beta, double *C, const int ldc )
-{
-    int m, n, k;
+int dgemm_scalaire(CBLAS_LAYOUT layout, CBLAS_TRANSPOSE transA,
+                   CBLAS_TRANSPOSE transB, const int M, const int N,
+                   const int K, const double alpha, const double *A,
+                   const int lda, const double *B, const int ldb,
+                   const double beta, double *C, const int ldc) {
+  int m, n, k;
 
-    if ( transA == CblasNoTrans ) {
-        if ( transB == CblasNoTrans ) {
-            for( n=0; n<N; n++ ) {
-                if ( beta != 1. ) {
-                    for( m=0; m<M; m++ ) {
-                        C[ ldc * n + m ] = beta * C[ ldc * n + m ];
-                    }
-                }
-                for( k=0; k<K; k++ ) {
-                    for( m=0; m<M; m++ ) {
-                        C[ ldc * n + m ] += alpha * A[ lda * k + m ] * B[ ldb * n + k ];
-                    }
-                }
-            }
+  if (transA == CblasNoTrans) {
+    if (transB == CblasNoTrans) {
+      for (n = 0; n < N; n++) {
+        if (beta != 1.) {
+          for (m = 0; m < M; m++) {
+            C[ldc * n + m] = beta * C[ldc * n + m];
+          }
         }
-        else {
-            for( m=0; m<M; m++ ) {
-                for( n=0; n<N; n++ ) {
-                    if ( beta != 1. ) {
-                        C[ ldc * n + m ] = beta * C[ ldc * n + m ];
-                    }
-                    for( k=0; k<K; k++ ) {
-                        C[ ldc * n + m ] += alpha * A[ lda * k + m ] * B[ ldb * k + n ];
-                    }
-                }
-            }
+        for (k = 0; k < K; k++) {
+          for (m = 0; m < M; m++) {
+            C[ldc * n + m] += alpha * A[lda * k + m] * B[ldb * n + k];
+          }
         }
+      }
+    } else {
+      for (m = 0; m < M; m++) {
+        for (n = 0; n < N; n++) {
+          if (beta != 1.) {
+            C[ldc * n + m] = beta * C[ldc * n + m];
+          }
+          for (k = 0; k < K; k++) {
+            C[ldc * n + m] += alpha * A[lda * k + m] * B[ldb * k + n];
+          }
+        }
+      }
     }
-    else {
-        if ( transB == CblasNoTrans ) {
-            for( m=0; m<M; m++ ) {
-                for( n=0; n<N; n++ ) {
-                    if ( beta != 1. ) {
-                        C[ ldc * n + m ] = beta * C[ ldc * n + m ];
-                    }
-                    for( k=0; k<K; k++ ) {
-                        C[ ldc * n + m ] += alpha * A[ lda * m + k ] * B[ ldb * n + k ];
-                    }
-                }
-            }
+  } else {
+    if (transB == CblasNoTrans) {
+      for (m = 0; m < M; m++) {
+        for (n = 0; n < N; n++) {
+          if (beta != 1.) {
+            C[ldc * n + m] = beta * C[ldc * n + m];
+          }
+          for (k = 0; k < K; k++) {
+            C[ldc * n + m] += alpha * A[lda * m + k] * B[ldb * n + k];
+          }
         }
-        else {
-            for( m=0; m<M; m++ ) {
-                for( n=0; n<N; n++ ) {
-                    if ( beta != 1. ) {
-                        C[ ldc * n + m ] = beta * C[ ldc * n + m ];
-                    }
-                    for( k=0; k<K; k++ ) {
-                        C[ ldc * n + m ] += alpha * A[ lda * m + k ] * B[ ldb * k + n ];
-                    }
-                }
-            }
+      }
+    } else {
+      for (m = 0; m < M; m++) {
+        for (n = 0; n < N; n++) {
+          if (beta != 1.) {
+            C[ldc * n + m] = beta * C[ldc * n + m];
+          }
+          for (k = 0; k < K; k++) {
+            C[ldc * n + m] += alpha * A[lda * m + k] * B[ldb * k + n];
+          }
         }
+      }
     }
+  }
 
-    return ALGONUM_SUCCESS;
+  return ALGONUM_SUCCESS;
+}
+
+void scale_block(const int M, const int N, const double beta, double *C,
+                 const int ldc) {
+  for (int bn = 0; bn < dgemm_seq_block_size; bn++) {
+    for (int bm = 0; bm < dgemm_seq_block_size; bm++) {
+      C[ldc * bn + bm] = beta * C[ldc * bn + bm];
+    }
+  }
+}
+
+int dgemm_bloc(CBLAS_LAYOUT layout, CBLAS_TRANSPOSE transA,
+               CBLAS_TRANSPOSE transB, const int M, const int N, const int K,
+               const double alpha, const double *A, const int lda,
+               const double *B, const int ldb, const double beta, double *C,
+               const int ldc) {
+  int m, n, k;
+
+  if (transA == CblasNoTrans) {
+    if (transB == CblasNoTrans) {
+      for (n = 0; n < N; n += dgemm_seq_block_size) {
+        if (beta != 1.) {
+          for (m = 0; m < M; m += dgemm_seq_block_size) {
+            scale_block(dgemm_seq_block_size, dgemm_seq_block_size, beta,
+                        C + ldc * n + m, ldc);
+          }
+        }
+        for (k = 0; k < K; k += dgemm_seq_block_size) {
+          for (m = 0; m < M; m += dgemm_seq_block_size) {
+            // C[ldc * n + m] += alpha * A[lda * k + m] * B[ldb * n + k];
+            dgemm_scalaire(layout, transA, transB, dgemm_seq_block_size,
+                           dgemm_seq_block_size, dgemm_seq_block_size, alpha,
+                           A + lda * k + m, lda, B + ldb * n + k, ldb, 1.0,
+                           C + ldc * n + m, ldc);
+          }
+        }
+      }
+    } else {
+      for (m = 0; m < M; m += dgemm_seq_block_size) {
+        for (n = 0; n < N; n += dgemm_seq_block_size) {
+          if (beta != 1.) {
+            scale_block(dgemm_seq_block_size, dgemm_seq_block_size, beta,
+                        C + ldc * n + m, ldc);
+          }
+          for (k = 0; k < K; k += dgemm_seq_block_size) {
+            dgemm_scalaire(layout, transA, transB, dgemm_seq_block_size,
+                           dgemm_seq_block_size, dgemm_seq_block_size, alpha,
+                           A + lda * k + m, lda, B + ldb * k + n, ldb, 1.0,
+                           C + ldc * n + m, ldc);
+          }
+        }
+      }
+    }
+  } else {
+    if (transB == CblasNoTrans) {
+      for (m = 0; m < M; m += dgemm_seq_block_size) {
+        for (n = 0; n < N; n += dgemm_seq_block_size) {
+          // if beta != 0, scale the C block before accumulating into it
+          if (beta != 1.) {
+            scale_block(dgemm_seq_block_size, dgemm_seq_block_size, beta,
+                        C + ldc * n + m, ldc);
+          }
+          for (k = 0; k < K; k += dgemm_seq_block_size) {
+            dgemm_scalaire(layout, transA, transB, dgemm_seq_block_size,
+                           dgemm_seq_block_size, dgemm_seq_block_size, alpha,
+                           A + lda * m + k, lda, B + ldb * n + k, ldb, 1.0,
+                           C + ldc * n + m, ldc);
+          }
+        }
+      }
+    } else {
+      for (m = 0; m < M; m += dgemm_seq_block_size) {
+        for (n = 0; n < N; n += dgemm_seq_block_size) {
+          // if beta != 0, scale the C block before accumulating into it
+          if (beta != 1.) {
+            scale_block(dgemm_seq_block_size, dgemm_seq_block_size, beta,
+                        C + ldc * n + m, ldc);
+          }
+          for (k = 0; k < K; k += dgemm_seq_block_size) {
+            dgemm_scalaire(layout, transA, transB, dgemm_seq_block_size,
+                           dgemm_seq_block_size, dgemm_seq_block_size, alpha,
+                           A + lda * m + k, lda, B + ldb * k + n, ldb, 1.0,
+                           C + ldc * n + m, ldc);
+          }
+        }
+      }
+    }
+  }
+
+  return ALGONUM_SUCCESS;
+}
+
+int dgemm_seq(CBLAS_LAYOUT layout, CBLAS_TRANSPOSE transA,
+              CBLAS_TRANSPOSE transB, const int M, const int N, const int K,
+              const double alpha, const double *A, const int lda,
+              const double *B, const int ldb, const double beta, double *C,
+              const int ldc) {
+  if (dgemm_seq_block_size != 1) {
+    dgemm_bloc(layout, transA, transB, M, N, K, alpha, A, lda, B, ldb, beta, C,
+               ldc);
+  } else {
+    dgemm_scalaire(layout, transA, transB, M, N, K, alpha, A, lda, B, ldb, beta,
+                   C, ldc);
+  }
+
+  return ALGONUM_SUCCESS;
 }
 
 /* To make sure we use the right prototype */
-static dgemm_fct_t valid_dgemm_seq __attribute__ ((unused)) = dgemm_seq;
+static dgemm_fct_t valid_dgemm_seq __attribute__((unused)) = dgemm_seq;
 
 /* Declare the variable that will store the information about this version */
 fct_list_t fct_dgemm_seq;
@@ -93,20 +196,18 @@ fct_list_t fct_dgemm_seq;
 /**
  * @brief Registration function
  */
-void dgemm_seq_init( void ) __attribute__( ( constructor ) );
-void
-dgemm_seq_init( void )
-{
-    fct_dgemm_seq.mpi    = 0;
-    fct_dgemm_seq.tiled  = 0;
-    fct_dgemm_seq.starpu = 0;
-    fct_dgemm_seq.name   = "seq";
-    fct_dgemm_seq.helper = "Sequential version of DGEMM";
-    fct_dgemm_seq.fctptr = dgemm_seq;
-    fct_dgemm_seq.next   = NULL;
+void dgemm_seq_init(void) __attribute__((constructor));
+void dgemm_seq_init(void) {
+  fct_dgemm_seq.mpi = 0;
+  fct_dgemm_seq.tiled = 0;
+  fct_dgemm_seq.starpu = 0;
+  fct_dgemm_seq.name = "seq";
+  fct_dgemm_seq.helper = "Sequential version of DGEMM";
+  fct_dgemm_seq.fctptr = dgemm_seq;
+  fct_dgemm_seq.next = NULL;
 
-    register_fct( &fct_dgemm_seq, ALGO_GEMM );
+  register_fct(&fct_dgemm_seq, ALGO_GEMM);
 
-    /* Read the value of dgemm_block_size */
-    dgemm_seq_block_size = myblas_getenv_value_int( "BLOCKSIZE", 32 );
+  /* Read the value of dgemm_block_size */
+  dgemm_seq_block_size = myblas_getenv_value_int("BLOCKSIZE", 1);
 }
